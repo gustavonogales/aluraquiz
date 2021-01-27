747023218c539a109b3fcbf17af43033c3ef52c7
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2021-01-26 23:33:49 -0300

    Add Eslint and first hooks

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import { Widget } from '../src/components/Widget';
 import { Footer } from '../src/components/Footer';
 import { GitHubCorner } from '../src/components/GitHubCorner';
 import { QuizBackground } from '../src/components/QuizBackground';
+import { useCallback, useState } from 'react';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
@@ -21,15 +19,33 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
+  const handleInputChange = useCallback((event) => {
+    setName(event.target.value);
+  });
+
+  const onHandleSubmit = useCallback((event) => {
+    event.preventDefault();
+    router.push(`/quiz?name=${name}`);
+  });
+
   return (
-    <QuizBackground backgroundImage={db.bg}> 
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>The legend of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, sit?</p>
+            <form onSubmit={onHandleSubmit}>
+              <input onChange={handleInputChange} 
+                placeholder='Preencha com seu nome'/>
+              <button type="submit" disabled={!!!name}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Footer />
